ad392a3616d918f0b180a62625300c40a4006a6c
kankan-web <2728360489@qq.com>
2024-03-28 23:41:47 +0800

    搭建基础的node+mock环境

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const mockList = require('./mock/index.js')
+const app = new Koa()
+const router = new Router()
+async function getRes(fn){
+  return new Promise(resolve=>{
+    setTimeout(()=>{
+      const res = fn()
+      resolve(res)
+    },1000)
+  })
+}
+//注册mock路由
+mockList.forEach(item => {
+  const {url,method,response} = item
+  router[method](url,async ctx=>{
+    // const res = response()
+    //模拟网络请求的加载状态，1s
+    const res = await getRes(response)
+    ctx.body = res
+  })
+});
+app.use(router.routes())
+app.listen(3000,()=>{
+  console.log('mock server is running at port 3000,http://localhost:3000/')
+})
---
+++ mock/index.js
@@ -0,0 +1,7 @@
+const question = require('./question.js')
+const test = require('./test.js')
+const mockList=[
+  ...question,
+  ...test
+]
+module.exports = mockList
